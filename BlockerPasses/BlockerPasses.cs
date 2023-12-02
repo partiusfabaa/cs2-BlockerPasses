41e93611bd3f0b8cb5167da7af30f6caf64a414b
hyo <96542489+partiusfabaa@users.noreply.github.com>
2023-12-02 13:13:23 +0500

    1

---
+++ BlockerPasses/BlockerPasses.cs
@@ -0,0 +1,201 @@
+﻿using System.Drawing;
+using System.Text.Json;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace BlockerPasses;
+
+[MinimumApiVersion(90)]
+public class BlockerPasses : BasePlugin
+{
+    public override string ModuleAuthor => "thesamefabius";
+    public override string ModuleName => "Blocker Passes";
+    public override string ModuleVersion => "v1.0.0";
+
+    private Config _config = null!;
+
+    public override void Load(bool hotReload)
+    {
+        _config = LoadConfig();
+        RegisterEventHandler<EventRoundStart>(EventRoundStart);
+    }
+
+    [RequiresPermissions("@css/root")]
+    [ConsoleCommand("css_bp_reload")]
+    public void OnCmdReload(CCSPlayerController? player, CommandInfo info)
+    {
+        _config = LoadConfig();
+
+        const string msg = "Configuration successfully rebooted";
+
+        if (player == null)
+            Console.WriteLine(msg);
+        else
+            player.PrintToChat(msg);
+    }
+
+    private HookResult EventRoundStart(EventRoundStart @event, GameEventInfo info)
+    {
+        var playersCount = Utilities.GetPlayers()
+            .Where(u => u.TeamNum != (int)CsTeam.None && u.TeamNum != (int)CsTeam.Spectator).ToList();
+
+        var configPlayers = _config.Players;
+        if (playersCount.Count > configPlayers) return HookResult.Continue;
+
+        if (!_config.Maps.TryGetValue(Server.MapName, out var entitiesMap)) return HookResult.Continue;
+
+        foreach (var entity in entitiesMap)
+        {
+            var color = entity.Color;
+
+            SpawnProp(entity.ModelPath, new[] { color[0], color[1], color[2] },
+                GetVectorFromString(entity.Origin), GetQAngleFromString(entity.Angles));
+        }
+
+        Server.PrintToChatAll(ReplaceColorTags(
+            _config.Message.Replace("{MINPLAYERS}", configPlayers.ToString())));
+
+        return HookResult.Continue;
+    }
+
+    private Vector GetVectorFromString(string vector) => GetFromString(vector, (x, y, z) => new Vector(x, y, z));
+
+    private QAngle GetQAngleFromString(string angles) => GetFromString(angles, (x, y, z) => new QAngle(x, y, z));
+
+    private T GetFromString<T>(string values, Func<float, float, float, T> createInstance)
+    {
+        var split = values.Split(' ');
+
+        if (split.Length >= 3 && 
+            float.TryParse(split[0], out var x) && 
+            float.TryParse(split[1], out var y) &&
+            float.TryParse(split[2], out var z))
+        {
+            return createInstance(x, y, z);
+        }
+
+        return default!;
+    }
+
+    private void SpawnProp(string propPath, int[] color, Vector origin, QAngle angles)
+    {
+        var prop = Utilities.CreateEntityByName<CBaseModelEntity>("prop_dynamic_override");
+
+        if (prop == null) return;
+
+        prop.Collision.SolidType = SolidType_t.SOLID_VPHYSICS;
+        prop.Render = Color.FromArgb(color[0], color[1], color[2]);
+        prop.Teleport(origin, angles, new Vector());
+        prop.DispatchSpawn();
+        Server.NextFrame(() => prop.SetModel(propPath));
+    }
+
+    private Config LoadConfig()
+    {
+        var configPath = Path.Combine(ModuleDirectory, "blocker_passes.json");
+        if (!File.Exists(configPath)) return CreateConfig(configPath);
+
+        var config = JsonSerializer.Deserialize<Config>(File.ReadAllText(configPath))!;
+
+        return config;
+    }
+
+    private Config CreateConfig(string configPath)
+    {
+        var config = new Config
+        {
+            Players = 6,
+            Message =
+                "[{BLUE} BlockerPasses {DEFAULT}] Some passageways are blocked. Unblocking requires {RED}{MINPLAYERS}{DEFAULT} players",
+            Maps = new Dictionary<string, List<Entities>>
+            {
+                {
+                    "de_mirage", new List<Entities>
+                    {
+                        new()
+                        {
+                            ModelPath =
+                                "models/props/de_dust/hr_dust/dust_windows/dust_rollupdoor_96x128_surface_lod.vmdl",
+                            Color = new[] { 30, 144, 255 },
+                            Origin = "-1600.46 -741.124 -172.965",
+                            Angles = "0 180 0"
+                        },
+                        new()
+                        {
+                            ModelPath = "models/props/de_mirage/small_door_b.vmdl",
+                            Color = new[] { 255, 255, 255 },
+                            Origin = "588.428 704.941 -136.517",
+                            Angles = "0 270.256 0"
+                        },
+                        new()
+                        {
+                            ModelPath = "models/props/de_mirage/large_door_c.vmdl",
+                            Color = new[] { 255, 255, 255 },
+                            Origin = "-1007.87 -359.812 -323.64",
+                            Angles = "0 270.106 0"
+                        },
+                        new()
+                        {
+                            ModelPath =
+                                "models/props/de_nuke/hr_nuke/chainlink_fence_001/chainlink_fence_001_256_capped.vmdl",
+                            Color = new[] { 255, 255, 255 },
+                            Origin = "-961.146 -14.2419 -169.489",
+                            Angles = "0 269.966 0"
+                        },
+                        new()
+                        {
+                            ModelPath =
+                                "models/props/de_nuke/hr_nuke/chainlink_fence_001/chainlink_fence_001_256_capped.vmdl",
+                            Color = new[] { 255, 255, 255 },
+                            Origin = "--961.146 -14.2419 -43.0083",
+                            Angles = "0 269.966 0"
+                        }
+                    }
+                }
+            }
+        };
+
+        File.WriteAllText(configPath,
+            JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true }));
+        return config;
+    }
+
+    private string ReplaceColorTags(string input)
+    {
+        string[] colorPatterns =
+        {
+            "{DEFAULT}", "{RED}", "{LIGHTPURPLE}", "{GREEN}", "{LIME}", "{LIGHTGREEN}", "{LIGHTRED}", "{GRAY}",
+            "{LIGHTOLIVE}", "{OLIVE}", "{LIGHTBLUE}", "{BLUE}", "{PURPLE}", "{GRAYBLUE}"
+        };
+        string[] colorReplacements =
+        {
+            "\x01", "\x02", "\x03", "\x04", "\x05", "\x06", "\x07", "\x08", "\x09", "\x10", "\x0B", "\x0C", "\x0E",
+            "\x0A"
+        };
+
+        for (var i = 0; i < colorPatterns.Length; i ++)
+            input = input.Replace(colorPatterns[i], colorReplacements[i]);
+
+        return input;
+    }
+}
+
+public class Config
+{
+    public int Players { get; init; }
+    public required string Message { get; init; }
+    public Dictionary<string, List<Entities>> Maps { get; init; } = null!;
+}
+
+public class Entities
+{
+    public string ModelPath { get; init; } = null!;
+    public int[] Color { get; init; } = null!;
+    public string Origin { get; init; } = null!;
+    public string Angles { get; init; } = null!;
+}
